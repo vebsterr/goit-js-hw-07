f4281b5a9e00257936346f9821ba5e53421d17ab
Vebster <vebster.khm@gmail.com>
2022-05-18 18:51:51 +0300

    Обновление домашки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js'
 
-console.log(galleryItems);
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery')
+const galleryMarkup = makeItemGalleryMarkup(galleryItems)
+let instance = null
+
+galleryContainer.innerHTML = galleryMarkup
+
+galleryContainer.addEventListener('click', onClickImages)
+
+function makeItemGalleryMarkup(listOfImages) {
+   return listOfImages
+      .map(({ preview, original, description }) => {
+         return `<div class="gallery__item">
+         <a class="gallery__link" href=${original}>
+            <img
+               class="gallery__image"
+               src=${preview}
+               data-source=${original}
+               alt=${description}
+            />
+         </a>
+      </div>`
+      })
+      .join('')
+}
+
+function onClickImages(event) {
+   const isImgEl = event.target.classList.contains('gallery__image')
+
+   if (!isImgEl) {
+      return
+   }
+
+   event.preventDefault()
+
+   galleryContainer.addEventListener('keydown', onEscModal)
+
+   const linkToOriginalSize = event.target.dataset.source
+
+   instance = basicLightbox.create(
+      `
+   <img src="${linkToOriginalSize}" alt="${event.target.alt}">
+   `
+   )
+
+   instance.show()
+}
+
+function onEscModal(event) {
+   event.preventDefault()
+   if (event.code === 'Escape') {
+      instance.close()
+      galleryContainer.removeEventListener('keydown', onEscModal)
+   }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+const galleryMarkup = makeItemGalleryMarkup(galleryItems)
+
+galleryContainer.innerHTML = galleryMarkup
+galleryContainer.addEventListener('click', onClickInPreviewPicture)
+
+function makeItemGalleryMarkup(listOfImages) {
+   return listOfImages
+      .map(({ preview, original, description }) => {
+         return `
+            <a class="gallery__item" href=${original}>
+               <img class="gallery__image" src=${preview} alt=${description} />
+            </a>
+            `
+      })
+      .join('')
+}
+
+function onClickInPreviewPicture(evt) {
+   evt.preventDefault()
+
+   const isImgEl = evt.target.classList.contains('gallery__image')
+
+   if (!isImgEl) {
+      return
+   }
+
+   const lightbox = new SimpleLightbox('.gallery a', {
+      docClose: true,
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+   })
+}
